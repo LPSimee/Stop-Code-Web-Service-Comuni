e563a03d76b45e326d47cec42bbb2c12f897205f
LP Simeon <lp.simeon@yahoo.com>
2025-10-07 21:10:49 +0200

    feat(Comune): modified properties and added lombok annotations

--- src/main/java/it/stopcode/rest_api_comuni/models/Comune.java
+++ src/main/java/it/stopcode/rest_api_comuni/models/Comune.java
@@ -1,10 +1,21 @@
 package it.stopcode.rest_api_comuni.models;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Comune {
     private int id;
     private String nome;
     private String codiceCatastale;
-    private String provincia;
-    private String regione;
     private String cap;
+    private String prefisso;
+    private String mail;
+    private String pec;
+    private String telefono;
+    private String fax;
+    private String[] coordinate;
 }
