dee3b8d601918df497d328e134e1af5aa5daf5cb
LP Simeon <lp.simeon@yahoo.com>
2025-10-07 21:03:08 +0200

    feat(files): pushed Unversioned files

---
+++ src/main/java/it/stopcode/rest_api_comuni/RestApiComuniApplication.java
@@ -0,0 +1,13 @@
+package it.stopcode.rest_api_comuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiComuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiComuniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/stopcode/rest_api_comuni/RestApiComuniApplicationTests.java
@@ -0,0 +1,13 @@
+package it.stopcode.rest_api_comuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiComuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
