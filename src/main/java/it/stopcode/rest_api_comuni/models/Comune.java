684856914521ca836134bcd56e8b752c16ea422d
LP Simeon <lp.simeon@yahoo.com>
2025-10-07 21:01:07 +0200

    feat(Comune): created "Comune" class and modified README file

---
+++ src/main/java/it/stopcode/rest_api_comuni/models/Comune.java
@@ -0,0 +1,10 @@
+package it.stopcode.rest_api_comuni.models;
+
+public class Comune {
+    private int id;
+    private String nome;
+    private String codiceCatastale;
+    private String provincia;
+    private String regione;
+    private String cap;
+}
