96460961af3cd1f538ce90576c96e59a0bb4b9fd
LP Simeon <lp.simeon@yahoo.com>
2025-10-08 15:46:32 +0200

    feat(data file): remove "schema.sql" file

--- src/main/java/it/stopcode/rest_api_comuni/controllers/ComuneController.java
+++ src/main/java/it/stopcode/rest_api_comuni/controllers/ComuneController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/comuni")
+@RequestMapping("/api/comuni")
 public class ComuneController {
     private final ComuneService comuneService;
 
--- src/main/java/it/stopcode/rest_api_comuni/repositories/ComuneRepository.java
+++ src/main/java/it/stopcode/rest_api_comuni/repositories/ComuneRepository.java
@@ -2,6 +2,7 @@ package it.stopcode.rest_api_comuni.repositories;
 
 import it.stopcode.rest_api_comuni.models.Comune;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
