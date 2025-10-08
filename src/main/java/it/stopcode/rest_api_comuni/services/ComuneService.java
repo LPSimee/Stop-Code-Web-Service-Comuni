53193d618574ee10d42ee4fec901cc09ac3bed75
LP Simeon <lp.simeon@yahoo.com>
2025-10-08 12:53:04 +0200

    feat(ComuneService): added "findAllComuni" method

--- src/main/java/it/stopcode/rest_api_comuni/services/ComuneService.java
+++ src/main/java/it/stopcode/rest_api_comuni/services/ComuneService.java
@@ -1,11 +1,18 @@
 package it.stopcode.rest_api_comuni.services;
 
+import it.stopcode.rest_api_comuni.models.Comune;
 import it.stopcode.rest_api_comuni.repositories.ComuneRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ComuneService {
     private final ComuneRepository comuneRepository;
 
     public ComuneService(ComuneRepository comuneRepository) { this.comuneRepository = comuneRepository; }
+
+    List<Comune> findAllComuni(){
+        return comuneRepository.findAll();
+    };
 }
