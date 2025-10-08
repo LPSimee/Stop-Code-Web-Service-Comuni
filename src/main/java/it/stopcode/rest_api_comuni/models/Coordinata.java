d1f59b3c672ed66b5e54242103e9590eafde53ac
LP Simeon <lp.simeon@yahoo.com>
2025-10-08 11:57:33 +0200

    refactor(Coordinata): renamed "Coordinate" class to "Coordinata" and added table annotation on Coordinata and Comune classes

---
+++ src/main/java/it/stopcode/rest_api_comuni/models/Coordinata.java
@@ -0,0 +1,20 @@
+package it.stopcode.rest_api_comuni.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "coordinate")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coordinata {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private double lat;
+    private double lng;
+}
--- src/main/java/it/stopcode/rest_api_comuni/models/Coordinate.java
+++
@@ -1,7 +0,0 @@
-package it.stopcode.rest_api_comuni.models;
-
-public class Coordinate {
-    private long id;
-    private double lat;
-    private double lng;
-}
