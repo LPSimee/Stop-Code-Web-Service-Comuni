dd6214f8f9c467a9b1418b22fb1cfd23a150888f
LP Simeon <lp.simeon@yahoo.com>
2025-10-08 14:09:53 +0200

    feat(ComuneController): added "getAllComuni" method

--- src/main/java/it/stopcode/rest_api_comuni/controllers/ComuneController.java
+++ src/main/java/it/stopcode/rest_api_comuni/controllers/ComuneController.java
@@ -1,9 +1,14 @@
 package it.stopcode.rest_api_comuni.controllers;
 
+import it.stopcode.rest_api_comuni.models.Comune;
 import it.stopcode.rest_api_comuni.services.ComuneService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/comuni")
@@ -12,4 +17,8 @@ public class ComuneController {
 
     public ComuneController(ComuneService comuneService){ this.comuneService = comuneService; }
 
+    @GetMapping
+    List<Comune> getAllComuni(){
+        return comuneService.findAllComuni();
+    };
 }
